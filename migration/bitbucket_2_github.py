2c209b59521053c516f5d044d9b929390de78c4c
zhangjianli <dew.maple@gmail.com>
2020-07-02 00:06:17 +0800

    finish migrate script from bitbucket to github, by cloning repos firstly

--- migration/bitbucket_2_github.py
+++ migration/bitbucket_2_github.py
@@ -1,15 +1,19 @@
 import json
+import os
 import sys
+import time
 from base64 import b64encode
 from collections import namedtuple
+from os import path, mkdir
+from os.path import expanduser
 
 import urllib3
 
-sys.path.append("../migrate_2_github")
+sys.path.append(path.dirname(path.dirname(__file__)))
 
 from migration.conf import config
 
-Github = namedtuple("Github", ["org", "user", "password"])
+Github = namedtuple("Github", ["org", "user", "password", "token"])
 BitBucket = namedtuple("BitBucket", ["workspace", "project", "user", "password"])
 
 http = urllib3.PoolManager()
@@ -26,7 +30,7 @@ def parse_repo_info(repos):
 
 def fetch_repositories():
     repositories = {}
-    page = 13
+    page = 1
     while 1:
         print(f"Fetch repos of page {page} ...")
         result = get_repo(page)
@@ -36,15 +40,13 @@ def fetch_repositories():
         if not repos:
             break
         repositories.update(parse_repo_info(repos))
-
-        with open(f"repository_{page:03}.json", 'w') as f:
-            json.dump(repositories, f)
-
         page += 1
         if page > result['size']:
             break
-
-    return repositories
+    repos_json_file = f"origin_repository_{page:03}.json"
+    with open(repos_json_file, 'w') as f:
+        json.dump(repositories, f)
+    return repositories, repos_json_file
 
 
 def get_repo(page):
@@ -58,27 +60,141 @@ def get_repo(page):
         print(e)
 
 
-def create_repository(repository):
+def create_repositories_from_json_file(json_file):
+    with open(json_file) as f:
+        repos = json.load(f)
+
+    all_repos = {}
+    for key, urls in repos.items():
+        time.sleep(2)
+        github_urls = check_exists(key)
+        if not github_urls:
+            github_urls = create_repository(key)
+        if github_urls:
+            print(f"Create repository success, {key}")
+            all_repos[key] = {
+                "origin_url": urls,
+                "github_url": github_urls
+            }
+        else:
+            print(f"Create repository failed, {key}")
+    merged_json_file = "all_repositories.json"
+    with open(merged_json_file, 'w') as f:
+        json.dump(all_repos, f)
+    return all_repos, merged_json_file
+
+
+def check_exists(repo_name):
     if github.org:
-        url = f"https://api.github.com/orgs/${github.org}/repos"
+        url = f"https://api.github.com/repos/{github.org}/{repo_name}"
     else:
-        url = f"https://api.github.com/user/${github.user}/repos"
+        url = f"https://api.github.com/repos/{github.user}/{repo_name}"
+    try:
+        resp = http.request("GET", url, headers={'Content-Type': 'application/json', "Authorization": f"Bearer {github.token}", "User-Agent": "I'm UA", })
+        if resp.status == 200:
+            result = json.loads(resp.data.decode('utf-8'))
+            return {
+                "git_url": result['git_url'],
+                "ssh_url": result['ssh_url'],
+            }
+    except Exception as e:
+        print(e)
+        return None
+
+
+def create_repository(repo_name):
+    if github.org:
+        url = f"https://api.github.com/orgs/{github.org}/repos"
+    else:
+        url = f"https://api.github.com/user/{github.user}/repos"
 
     data = {
-        "name": repository.slug,
-        "description": repository.description,
-        "private": repository.is_private,
-        "has_issues": repository.has_issues,
-        "has_wiki": repository.has_wiki
+        "name": repo_name,
+        "description": "",
+        "private": True,
+        "has_issues": False,
+        "has_wiki": False
     }
-    # requests.post(url, )
+    encoded_data = json.dumps(data).encode('utf-8')
+    try:
+        resp = http.request("POST", url, headers={'Content-Type': 'application/json', "Authorization": f"Bearer {github.token}", "User-Agent": "I'm UA", }, body=encoded_data)
+        if resp.status == 200:
+            result = json.loads(resp.data.decode('utf-8'))
+            return {
+                "git_url": result['git_url'],
+                "ssh_url": result['ssh_url'],
+            }
+        else:
+            print(json.loads(resp.data.decode('utf-8')))
+            return None
+    except Exception as e:
+        print(e)
+
+
+def _repo_path(repo_name):
+    home = expanduser("~")
+    repo_path_ = path.join(home, f"repo-migration/{bit_bucket.workspace}/{bit_bucket.project}")
+    if not path.exists(repo_path_):
+        mkdir(repo_path_)
+    repo = path.join(repo_path_, repo_name)
+    if not path.exists(repo):
+        mkdir(repo)
+    return repo
+
+
+def pull(repo_path, url):
+    try:
+        r = os.system(f"cd {repo_path} && git clone --bare {url} && cd ../")
+        if r == 0:
+            print(f"Pull {url} done")
+            return True
+    except Exception as e:
+        print(e)
+    return False
+
+
+def push(repo_path, url):
+    try:
+        url = url.replace("git://", "https://")
+        fname = path.split(repo_path)[-1]
+        repo_path = f"{repo_path}/{fname}.git"
+        r = os.system(f"cd {repo_path} && git push --mirror {url} && cd ../")
+        if r == 0:
+            print(f"Push {url} done")
+            return True
+    except Exception as e:
+        print(e)
+    return False
+
+
+def migrate(repos_json):
+    with open(repos_json) as f:
+        repos = json.load(f)
+
+    failed = []
+    for name, urls in repos.items():
+        repo_path = _repo_path(name)
+        origin_url = urls['origin_url'][0]['href']
+        github_url = urls['github_url']['git_url']
+        print(f"Start to pull {name}: {origin_url} ")
+        if pull(repo_path, origin_url):
+            print(f"Start to push {name}: {github_url}")
+            push(repo_path, github_url)
+        else:
+            failed.append({repo_path: [origin_url, github_url]})
+        print("\n")
+
+    if failed:
+        print(f"Failed, {failed}")
 
 
 if __name__ == '__main__':
     github_conf = config['github']
-    github = Github(github_conf['org'], github_conf['user'], github_conf['password'])
+    github = Github(github_conf['org'], github_conf['user'], github_conf['password'], github_conf['token'])
 
     bitbucket_conf = config['bitbucket']
     bit_bucket = BitBucket(bitbucket_conf["workspace"], bitbucket_conf['project'], bitbucket_conf['user'], bitbucket_conf['password'])
 
-    fetch_repositories()
+    _, json_file = fetch_repositories()
+    _, all_json_file = create_repositories_from_json_file(json_file)
+    migrate(all_json_file)
